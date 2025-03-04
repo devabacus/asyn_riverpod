e86b2359d00f7eee6e7a3297df4245ac59159852
mrfrolk <ekaterita20@gmail.com>
2025-03-04 09:50:14 +0500

    1

--- lib/counter.dart
+++ lib/counter.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/material.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'counter.g.dart';
 
--- lib/counter_screen.dart
+++ lib/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:asyn_riverpod/counter.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 class CounterScreen extends HookConsumerWidget {
   const CounterScreen({super.key});
@@ -20,10 +21,30 @@ class AdvancedScreen extends HookConsumerWidget {
   const AdvancedScreen({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     final counter = ref.watch(counterProvider);
     final localCounter = useState(50);
 
-    return Center(child: Text("$localCounter", style: TextStyle(fontSize: 30)));
+    useEffect(() {
+      print('Глобальный счетчик ${counter}, локальный счетчик ${localCounter.value}');
+      return null;
+    }, [counter, localCounter]);
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("global: ${counter}", style: TextStyle(fontSize: 30)),
+        SizedBox(height: 30),
+        Text("local: ${localCounter.value}", style: TextStyle(fontSize: 30)),
+        SizedBox(height: 30),
+        ElevatedButton(
+          onPressed: () => ref.read(counterProvider.notifier).state++,
+          child: Text("+ global"),
+        ),
+        ElevatedButton(
+          onPressed: () => localCounter.value++,
+          child: Text("+ local"),
+        ),
+      ],
+    );
   }
 }
